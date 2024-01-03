e44073bcafbc755ec5a8205528bee2ed6ca73b69
AnnaAdM <anna.alimonda@gmail.com>
2024-01-03 15:30:27 +0100

    completed game

--- main.py
+++ main.py
@@ -5,35 +5,36 @@ from itertools import permutations
 
 
 def clear():
-    go_again=True
-    turn = 1
+    clear_turn = 1
+    clear_field = art.field
     pc_turns.clear()
     player_turns.clear()
     exclude_values.clear()
-    return field,turn,go_again
+    return clear_turn, clear_field
 
 
 def play_again():
-    again = input("Do You Want to play again? Y/N").upper()
+    again = input("Do You Want to play again? Y/N ").upper()
     if again == "Y":
-        clear()
+        return True
     else:
-        go_again = False
-        return go_again
+        print("Goodbye! Thanks for playing.")
+        return False
 
 
 def check_winner(player_moves, pc_moves):
     player_permutations = list(permutations(player_moves, 3))
-    pc_permutations = list(permutations(pc_moves,3))
-    for perm in player_permutations :
+    pc_permutations = list(permutations(pc_moves, 3))
+    for perm in player_permutations:
         if list(perm) in winning_turns:
             print("😎🏆You Win!!!🏆😎")
-            play_again()
-
+            return True
     for perm in pc_permutations:
         if list(perm) in winning_turns:
             print("😒🫂You Lose!!!🫂😒")
-            play_again()
+            return True
+
+    return False
 
 
 def choose_symbol():
@@ -49,16 +50,14 @@ def choose_symbol():
     else:
         print(f"please choose a valid letter (0_0)")
 
-    return player,pc
+    return player, pc
 
-#-------------------------------------------------------------------------------------
-go_again = True
-pc = ""
-field = art.field
+
+# -------------------------------------------------------------------------------------
+
 player_turns = []
 pc_turns = []
 exclude_values = []
-turn = 1
 winning_turns = [
     # Rows
     [1, 2, 3],
@@ -72,51 +71,47 @@ winning_turns = [
     [1, 5, 9],
     [3, 5, 7]
 ]
-#------------------------------------------- Start -------------------------------------------
+# ------------------------------------------- Start -------------------------------------------
 print(f"Welcome to the {art.logo}  game!")
-
-
-while go_again:
-    turn= 1
-    go_again = True
-    field = art.field
+turn = 1
+while True:
+    turn, field = clear()
     player, pc = choose_symbol()
-    while turn < 5 and go_again:
-        player_turn = input(f"Where? Choose a place for your {player}\n{field}")
+
+    while turn <= 4:
+        player_turn = input(f"Where? Choose a place for your {player} \n{field}")
 
         if not player_turn.isnumeric() or int(player_turn) > 9:
             print(f"please choose a valid number (1-9)")
-        elif int(player_turn) in pc_turns:
+        elif int(player_turn) in pc_turns or int(player_turn) in player_turns:
             print("Already taken! Please choose a different position!")
+
         else:
             player_turns.append(int(player_turn))
             new_field = field.replace(str(player_turn), player)
             print(new_field)
             print(f"your moves: {player_turns}")
 
-            check_winner(player_turns, pc_turns)
-
             exclude_values = player_turns + pc_turns
+            game_ended = check_winner(player_turns, pc_turns)
+            if game_ended:
+                break
+
             pc_turn = random.choice([x for x in range(1, 10) if x not in exclude_values])
             print(f"Computer put its {pc} in {pc_turn}")
             pc_turns.append(pc_turn)
             print(f"Pc moves: {pc_turns}")
-            check_winner(player_turns, pc_turns)
-
+            game_ended = check_winner(player_turns, pc_turns)
+            if game_ended:
+                break
             new_field = new_field.replace(str(pc_turn), pc)
             print(new_field)
+            field = new_field
             turn += 1
+
     else:
         print("Nobody wins..")
-        play_again()
 
-else:
-    print("Goodbye! Thanks for playing.")
-
-
-
-# TODO reset field
-# todo svuotare le liste dopo play again
-
-
-
+    play_again_option = play_again()
+    if not play_again_option:
+        break
